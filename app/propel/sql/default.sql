ad81644036cbe67e46735613edac4158affc100f
Musak <musak.istvan@gmail.com>
2012-11-14 21:49:24 +0200

    add propel and other

---
+++ app/propel/sql/default.sql
@@ -0,0 +1,104 @@
+
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ---------------------------------------------------------------------
+-- book
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `book`;
+
+CREATE TABLE `book`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(100),
+    `ISBN` VARCHAR(20),
+    `author_id` INTEGER,
+    PRIMARY KEY (`id`),
+    INDEX `book_FI_1` (`author_id`),
+    CONSTRAINT `book_FK_1`
+        FOREIGN KEY (`author_id`)
+        REFERENCES `author` (`id`)
+) ENGINE=MyISAM;
+
+-- ---------------------------------------------------------------------
+-- author
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `author`;
+
+CREATE TABLE `author`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(100),
+    `last_name` VARCHAR(100),
+    PRIMARY KEY (`id`)
+) ENGINE=MyISAM;
+
+-- ---------------------------------------------------------------------
+-- fos_user
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `fos_user`;
+
+CREATE TABLE `fos_user`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(255),
+    `username_canonical` VARCHAR(255),
+    `email` VARCHAR(255),
+    `email_canonical` VARCHAR(255),
+    `enabled` TINYINT(1) DEFAULT 0,
+    `salt` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `last_login` DATETIME,
+    `locked` TINYINT(1) DEFAULT 0,
+    `expired` TINYINT(1) DEFAULT 0,
+    `expires_at` DATETIME,
+    `confirmation_token` VARCHAR(255),
+    `password_requested_at` DATETIME,
+    `credentials_expired` TINYINT(1) DEFAULT 0,
+    `credentials_expire_at` DATETIME,
+    `roles` TEXT,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `fos_user_U_1` (`username_canonical`),
+    UNIQUE INDEX `fos_user_U_2` (`email_canonical`)
+) ENGINE=MyISAM;
+
+-- ---------------------------------------------------------------------
+-- fos_group
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `fos_group`;
+
+CREATE TABLE `fos_group`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `roles` TEXT,
+    PRIMARY KEY (`id`)
+) ENGINE=MyISAM;
+
+-- ---------------------------------------------------------------------
+-- fos_user_group
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `fos_user_group`;
+
+CREATE TABLE `fos_user_group`
+(
+    `fos_user_id` INTEGER NOT NULL,
+    `fos_group_id` INTEGER NOT NULL,
+    PRIMARY KEY (`fos_user_id`,`fos_group_id`),
+    INDEX `fos_user_group_FI_2` (`fos_group_id`),
+    CONSTRAINT `fos_user_group_FK_1`
+        FOREIGN KEY (`fos_user_id`)
+        REFERENCES `fos_user` (`id`),
+    CONSTRAINT `fos_user_group_FK_2`
+        FOREIGN KEY (`fos_group_id`)
+        REFERENCES `fos_group` (`id`)
+) ENGINE=MyISAM;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
